cacaa4497b37140327d4cebfd64c59ca97867f56
Jeanette <jbrasepae@gmail.com>
2020-12-10 16:42:26 +0200

    fixed

--- src/github_api.js
+++ src/github_api.js
@@ -1,26 +1,24 @@
 const fetch = require('node-fetch');
 function getRepos(repoName, startDate, endDate) {
-	var githubUser = 'freeCodeCamp';
 	var start_dates = new Date(startDate);
 	var end_dates = new Date(endDate);
 
-	fetch(`https://api.github.com/repos/${githubUser}/${repoName}/pulls
+	fetch(`https://api.github.com/repos/${repoName}/pulls
 		  `)
 		.then((response) => response.json())
 		.then((data) => {
-			i = 0;
-			for (i in data) {
-				if (
-					(new Date(data[i].created_at) >= start_dates && new Date(data[i].created_at) <= end_dates) ||
+			for (let i = 0; i in data; i++) {
+
+				if ((new Date(data[i].created_at) >= start_dates && new Date(data[i].created_at) <= end_dates) ||
 					(new Date(data[i].updated_at) >= start_dates && new Date(data[i].updated_at) <= end_dates) ||
 					(new Date(data[i].closed_at) >= start_dates && new Date(data[i].closed_at) <= end_dates) ||
-					(new Date(data[i].merged_at) >= start_dates && new Date(data[i].merged_at) <= end_dates)
-				) {
-					console.log(data[i]);
+					(new Date(data[i].merged_at) >= start_dates && new Date(data[i].merged_at) <= end_dates)) {
+
+                    console.log(data[i]);
+					//console.log(`createAt ${data[i].created_at} updatedAt ${data[i].updated_at}`);
 				}
-				i++;
 			}
 		})
 		.catch((error) => console.error(error));
 }
-getRepos('boilerplate-project-stockchecker', '2018-06-18', '2020-03-27');
+getRepos('freeCodeCamp/boilerplate-project-stockchecker', '2018-06-18', '2020-03-27');
